5321c70468b59abd5b2cb822ab78123d6856ec49
Ian <gichaneian@gmail.com>
2023-09-20 18:59:00 +0300

    Completed callback...I kinda understand

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callBack){
+
+
+
+
+
+return callBack()
+    
+}
+
+function returnsANamedFunction(){
+  return function delta(){
+
+  }
+}
+function returnsAnAnonymousFunction(){
+
+  return function(){}
+}
